9b4f70705f287cceb85b88f14d61f299ed23ba33
Tettu0530 <89886910+Tettu0530@users.noreply.github.com>
2023-01-18 19:13:19 +0900

    Add files via upload

--- paylink_gen.py
+++ paylink_gen.py
@@ -1,11 +1,9 @@
+# -*- coding: utf-8 -*-
 import os
 import random
-import sys
-from turtle import color
 
 import requests
 import ctypes
-import datetime
 import uuid
 import time
 
@@ -51,22 +49,30 @@ def rand_gen(n):
         ",")
     return "".join(random.choice(STRING) for i in range(n))
 
+
 def check_link(code_i, file):
     code = code_i.replace("https://pay.paypay.ne.jp/", "")
     client_uuid = str(uuid.uuid4())
     p2pinfo_headers = {
-        "Accept":"application/json, text/plain, */*",
+        "Accept": "application/json, text/plain, */*",
         "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36"
     }
-    get_p2p = requests.get(f"https://www.paypay.ne.jp/app/v2/p2p-api/getP2PLinkInfo?verificationCode={code}&client_uuid={client_uuid}", headers=p2pinfo_headers)
+    get_p2p = requests.get(
+        f"https://www.paypay.ne.jp/app/v2/p2p-api/getP2PLinkInfo?verificationCode={code}&client_uuid={client_uuid}", headers=p2pinfo_headers)
     data_p2p = get_p2p.json()
     try:
         if data_p2p["payload"]["orderStatus"] == "PENDING":
             try:
                 amont = data_p2p["payload"]["pendingP2PInfo"]["amount"]
-                print(f"{Color.GREEN}[SUCCESS] https://pay.paypay.ne.jp/{code} | Amount:{str(amont)} | {Color.GREEN}RESCODE:{str(get_p2p.status_code)} {Color.RESET}")
+                if data_p2p["payload"]["pendingP2PInfo"]["isSetPasscode"] == True:
+                    print(
+                        f"{Color.GREEN}[SUCCESS] https://pay.paypay.ne.jp/{code} | 金額t:{str(amont)} | {Color.GREEN}レスポンスコード:{str(get_p2p.status_code)} | {Color.RED}パスワード:あり {Color.RESET}")
+                else:
+                    print(
+                        f"{Color.GREEN}[SUCCESS] https://pay.paypay.ne.jp/{code} | 金額:{str(amont)} | {Color.GREEN}レスポンスコード:{str(get_p2p.status_code)} | {Color.GREEN}パスワード:なし {Color.RESET}")
             except:
-                print(f"{Color.GREEN}[SUCCESS] https://pay.paypay.ne.jp/{code} | {Color.GREEN}RESCODE:{str(get_p2p.status_code)} {Color.RESET}")
+                print(
+                    f"{Color.GREEN}[SUCCESS] https://pay.paypay.ne.jp/{code} | {Color.GREEN}レスポンスコード:{str(get_p2p.status_code)} {Color.RESET}")
         try:
             with open(f"{file}.txt", "r", encoding="utf-8") as f:
                 content = f.read()
@@ -77,19 +83,22 @@ def check_link(code_i, file):
                     f.write(f"https://pay.paypay.ne.jp/{code}")
                 else:
                     f.write(f"\nhttps://pay.paypay.ne.jp/{code}")
-                return("success")
+                return ("success")
         else:
-            print(f"{Color.RED}[FAILURE] https://pay.paypay.ne.jp/{code} | The URL dos not pending | {Color.GREEN}RESCODE:{str(get_p2p.status_code)} {Color.RESET}")
-            return("died")
+            print(
+                f"{Color.RED}[FAILURE] https://pay.paypay.ne.jp/{code} | URLは無効です | {Color.GREEN}レスポンスコード:{str(get_p2p.status_code)} {Color.RESET}")
+            return ("died")
     except:
-        print(f"{Color.RED}[FAILURE] https://pay.paypay.ne.jp/{code} | {Color.YELLOW}The URL does not exist | {Color.GREEN}RESCODE:{str(get_p2p.status_code)} {Color.RESET}")
-        return("died")
+        print(f"{Color.RED}[FAILURE] https://pay.paypay.ne.jp/{code} | {Color.YELLOW}URLは無効です | {Color.GREEN}レスポンスコード:{str(get_p2p.status_code)} {Color.RESET}")
+        return ("died")
 
 
 os.system("cls")
 os.system("title PayPayLinkGenerator-Checker Made by Tettu0530#0530-TuVoNeX#2214")
 
-print(Color.BLUE + f"""  _____            _____            _      _       _                
+def main():
+    os.system("cls")
+    print(Color.BLUE + f"""  _____            _____            _      _       _                
  |  __ \          |  __ \          | |    (_)     | |               
  | |__) __ _ _   _| |__) __ _ _   _| |     _ _ __ | | __            
  |  ___/ _` | | | |  ___/ _` | | | | |    | | '_ \| |/ /            
@@ -103,48 +112,72 @@ print(Color.BLUE + f"""  _____            _____            _      _       _
  | |__| |  __| | | | | (_>  < | |____| | | |  __| (__|   |  __| |   
   \_____|\___|_| |_|  \___/\/  \_____|_| |_|\___|\___|_|\_\___|_|  
 
-{Color.RESET}---------- Author:{Color.RED}Tettu0530#0530&TuVoNeX#2214{Color.RESET} ---------
--------------------- Version:{Color.RED}1.1.0{Color.RESET} --------------------
-""")
+{Color.RESET}----------- 作者:{Color.RED}Tettu0530#0530&TuVoNeX#2214{Color.RESET} ----------
+-------------------- Version:{Color.RED}1.2.1{Color.RESET} --------------------
+    """)
+    print("""
+[1] PayPayLinkGenerator
+[2] PayPayLinkChecker
+[3] Exit
+    """)
+    number = input(f"[{Color.RED}!{Color.RESET}]モジュールを選択してください >")
+    if number == "1":
+        amount = input(
+            f"{Color.CYAN}生成するリンク数:{Color.RESET}")
+        delay_gen = input(
+            f"{Color.CYAN}リンクを生成する間隔(推奨:0):{Color.RESET}")
+        
+        li = []
 
-amount = input(f"{Color.CYAN}How many links do you want to generate?:{Color.RESET}")
-delay_gen = input(f"{Color.CYAN}Please input delay of generating urls. (0.001 is recommended.):{Color.RESET}")
-yesno = input(f"{Color.CYAN}Do you want to check the PayPay link?(Y/N):{Color.RESET}")
-print(f"Ganarating {str(amount)} of PayPay links...")
+        for i in range(int(amount)):
+            link_i = rand_gen(16)
+            link = "https://pay.paypay.ne.jp/" + link_i
+            li.append(link)
+            print(f"リンクを生成しました : {link}")
+            time.sleep(float(delay_gen))
+        with open(f"paylink.txt", "w", encoding="utf-8") as f:
+            for i_ in li:
+                f.write(f"{i_}\n")
+        time.sleep(1)
+        print(f"{str(amount)} 個リンクを生成しました。")
+        input("なんらかのキーを押すと終了します")
+        main()
+    elif number == "2":
+        success = 0
+        failure = 0
+        total = 0
+        file = input("リンクの入ったファイルの名前指定してください(例:link.txt)(空欄の場合はこのツールで生成したリンクがチェックされます):")
+        if file == "":
+                files = "paylink.txt"
+        else:
+            files = file
+        if os.path.isfile(f"{files}") is True:
+            delay = input("チェック間隔を設定してください(強く推奨:3以上):")
+            with open(files, "r") as f:
+                l = f.read().split("\n")
+                paylink = ([i for i in l if i != ""])
+                for i in paylink:
+                    result = check_link(i, "success_link.txt")
+                    time.sleep(int(delay))
+                    if result == "success":
+                        success += 1
+                    elif result == "died":
+                        failure += 1
+                print(f"""
+{Color.UNDERLINE} {str(success + failure)} 個のリンクをチェックしました{Color.RESET}
+{Color.GREEN}[SUCCESS] 合計: {str(success)} 個{Color.RESET}
+{Color.RED}[FAILURE] 合計 : {str(failure)} 個{Color.RESET}
+                """)
+                enter = input("Enterキーを押すと終了します")
+                if enter == "":
+                    main()
+        else:
+            print(f"{Color.RED}[エラー]指定したファイル({files})は見つかりませんでした。{Color.RESET}")
+            time.sleep(1)
+            input("なんらかのキーを押すと終了します")
+            main()
 
-li = []
-success = 0
-failure = 0
-total = 0
-
-for i in range(int(amount)):
-    link_i = rand_gen(16)
-    link = "https://pay.paypay.ne.jp/" + link_i
-    li.append(link)
-    print(f"Successfully generated link : {link}")
-    time.sleep(float(delay_gen))
-with open(f"paylink.txt", "w", encoding="utf-8") as f:
-    for i_ in li:
-        f.write(f"{i_}\n")
-
-if yesno == "Y" or yesno == "y":
-    delay = input("Please input delay. (3 seconds is recommended.):")
-    with open(f"paylink.txt", "r") as f:
-        l = f.read().split("\n")
-        paylink = ([i for i in l if i != ""])
-        for i in paylink:
-            result = check_link(i, "success_link.txt")
-            time.sleep(int(delay))
-            if result == "success":
-                success += 1
-            elif result == "died":
-                failure += 1
-        print(f"""
-{Color.UNDERLINE}Successfully checked {str(success + failure)} URL.{Color.RESET}
-{Color.GREEN}[SUCCESS] Total : {str(success)} URLS{Color.RESET}
-{Color.RED}[FAILURE] Total : {str(failure)} URLS{Color.RESET}
-        """)
+    elif number == "3":
+        print("ツールを終了します...")
         os.system("PAUSE")
-else:
-    print("exit application...")
-    os.system("PAUSE")
+main()
